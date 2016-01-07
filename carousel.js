80085da4576ee87fe975c697b4084a34459a0635
Travis Tucker <t.tuck303@gmail.com>
2016-01-06 21:46:11 -0700

    Figured out jq animate with css to move an image off the screen to the left.

--- carousel.js
+++ carousel.js
@@ -1,10 +1,13 @@
 $( document ).ready(function(){
+  console.log("page loaded");
 
-  var slide_interval = 5000; // 5sec
-  var carousel_timer = window.setTimeout(slide_left(), slide_interval);
+  var slide_interval = 2000; // 2sec
+  var carousel_timer = window.setInterval(slide_left, slide_interval);
 
-})
+});
 
 var slide_left = function(){
-  
-}
+  console.log("attempting to slide to the left...");
+  $('.current_image').animate({'left': '-=801px'});
+  console.log( $('.current_image').css('left'));
+};
